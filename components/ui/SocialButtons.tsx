40dc4a8d434bf9e6049377a16a396c17aa79d450
Ferhat <your@email.com>
2026-07-02 13:45:57 +0300

    Add Home icon to social buttons in bottom-right corner

--- components/ui/SocialButtons.tsx
+++ components/ui/SocialButtons.tsx
@@ -2,6 +2,16 @@
 
 const links = [
   {
+    href:  "/",
+    label: "Home",
+    icon:  (
+      <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+        <polyline points="9 22 9 12 15 12 15 22"/>
+      </svg>
+    ),
+  },
+  {
     href:  "https://x.com/dexar_app",
     label: "X (Twitter)",
     icon:  (
